6586fe993847bd76ec073c755bfdd83ae0d05d7b
belayaanna13 <89985036+belayaanna13@users.noreply.github.com>
2022-01-24 13:48:57 +0300

    massage

--- task.py
+++ task.py
@@ -1,7 +1,11 @@
+import time
+
+time_from_start = time.time()
+
 m = [0, 1]
 c = 2
 n = 5 #количество чисел в массиве
-x = 0.3
+x = 0.137
 arr = [0.11, 0.137, 0.367, 0.872, 0.987]
 
 hash_table = {
@@ -27,4 +31,27 @@ if c == 1:
     print(f'Число {x} найдено в массиве {arr}')
 else:
     print(f'Число {x} не найдено в массиве {arr}')
+
+time_of_program = time.time() - time_from_start
+print(time_of_program)
+
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({'Time': [time_of_program],
+                   'n': [n]
+})
+
+
+#Способ_1
+df.to_excel('./task.xlsx')
+np.savetxt('./task.xlsx', df)
+
+#Способ_2
+# sheet = 'Лист1'
+# df.reset_index(level=0, inplace=True)
+# with pd.ExcelWriter('./task.xlsx') as writer:
+#     df.to_excel(writer, sheet_name = sheet, header=None, index=None)
+
+
 
